b606c4bd43adfec9d3157571f111e0bbfbd905c0
William Lambert <william-lambert@live.fr>
2018-10-27 23:29:29 +0200

    Will1

--- Untitled10.py
+++ Untitled10.py
@@ -24,7 +24,7 @@ print(d3)
 
 
 
-mots = ['eddueardo999', 'catelyn999', 'robb', 'sansa', 'arya', 'brandon',
+mots = ['eddueardo111', 'catelyn111', 'robb', 'sansa', 'arya', 'brandon',
 
 
         'rickon', 'theon', 'rorbert', 'cersei', 'tywin', 'jaime',
